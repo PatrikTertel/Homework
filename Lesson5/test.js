2329e62c84d029db3fa8775922921de82fd4735c
Patrik Tertel <pponiatovskiy@gmail.com>
2023-06-06 15:13:28 +0300

    Lesson5 homework

---
+++ Lesson5/test.js
@@ -0,0 +1,41 @@
+const a = "Text";
+
+const b = true;
+
+const c = 5;
+
+const d = "666#";
+
+console.log(a + b); //string + boolean
+
+console.log(a + c); //string + number
+
+console.log(c + b); //number + boolean
+
+console.log(a * b); //string * boolean
+
+console.log(a * c); //string * number
+
+console.log(c * b); //number * boolean
+
+console.log(a / b); //string / boolean
+
+console.log(a / c); //string / number
+
+console.log(c / b); //number / boolean
+
+console.log(parseInt("42")) // 42
+
+console.log(Number(true)) // 1
+
+console.log(Number(false)) // 0
+
+console.log(String(42)) // "42"
+
+console.log(String(true)) // "true"
+
+console.log(String(false)) // "false"
+
+console.log(Boolean("hello")) // true
+
+console.log(Boolean(42)) // true
